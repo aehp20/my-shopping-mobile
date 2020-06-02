ed8a64cdf06a615ce60e8af7f232ceccf5fc4d3c
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2020-06-02 22:01:43 +0200

    Remove Menu files

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -10,25 +10,21 @@ import {
 
 import { ListsProducts } from '../listsProducts'
 import { StyledHomeTitle } from './Home-Styles'
-import { Menu } from './menu'
 
 export function Home() {
   return (
-    <>
-      <Menu />
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton />
-            </IonButtons>
-            <StyledHomeTitle>My shopping</StyledHomeTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          <ListsProducts />
-        </IonContent>
-      </IonPage>
-    </>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+            <IonMenuButton />
+          </IonButtons>
+          <StyledHomeTitle>My shopping</StyledHomeTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <ListsProducts />
+      </IonContent>
+    </IonPage>
   )
 }
