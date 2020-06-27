f976acfd27295642c6ab7de7abdeb3a67b7159c9
Aron Herrera Ponte <aherrera@MacBook-Pro.local>
2020-06-27 08:44:57 +0200

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
-import { IonApp, IonRouterOutlet, setupConfig } from '@ionic/react'
+import { IonApp, IonRouterOutlet } from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
 import { ThemeProvider } from 'styled-components'
 
@@ -40,10 +40,6 @@ import '@ionic/react/css/display.css'
 
 /* Theme variables */
 import './theme/variables.css'
-
-setupConfig({
-  hardwareBackButton: false,
-})
 
 const App: React.FC = () => (
   <IonApp>
